64eafbfd6379d08ed25e80397c7ca1dae88f693f
Rajiv <1998irajeev@gmail.com>
2021-05-30 12:28:22 +0530

    added toggling theme funtionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import Header from './Header/Header'
 import './style.scss'
 import './styles.css'
 import 'tailwindcss/tailwind.css'
+import { ThemeProvider } from './theme/themeContext.js'
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider>
       <Header />
       <h1>React TypeScript Webpack Starter Template</h1>
       <p style={{ color: 'red' }}>ss</p>
-    </>
+    </ThemeProvider>
   )
 }
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import ThemeToggle from '../ThemeToggle/ThemeToggle.js'
 
 const Header = () => {
-  return <div className="bg-white w-full p-4">Header</div>
+  return (
+    <div className="bg-primary w-full p-4">
+      Header <ThemeToggle />
+    </div>
+  )
 }
 
 export default Header
